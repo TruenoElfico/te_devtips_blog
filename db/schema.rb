68bdc8a94ffc6d1e57576fa9f634dae2b3455dda
Luis Enrique <1234luis.tux@gmail.com>
2016-03-10 19:40:42 -0700

    add animals

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308193113) do
+ActiveRecord::Schema.define(version: 20160311023854) do
+
+  create_table "animals", force: :cascade do |t|
+    t.string   "name"
+    t.string   "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
